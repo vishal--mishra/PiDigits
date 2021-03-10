50a9f77a8647b5abcf4025fecb5313bc9a544cbe
vishal--mishra <vmishra@gmail.com>
2021-03-09 20:41:33 -0800

    Create pi257.py
    Output from the script -
    
    Here is how to calculate real radical form expression for cos(pi * [1,2,4,8,16,32,64,128,256] / 257) -
    Calculate: t[2] => z[4] => y[8] => x[16] => v[12] => u[8] => w[9]
    t = [ 7.51560977094069868243567737885 | -8.51560977094069868243567737885 ] 
    z = [ 9.2460739711898920587441732180 | 1.58418984601778977666821607178 | -1.73046420024919337630849583912 | -10.0997996169584884591038934506 ] 
    y = [ 11.8604556333932954115902083001 | 0.262770553504383985656868516285 | 2.26579136793314561525163092203 | -6.3864173245713573856569256391 | -2.6143816622034033528460350821 | 1.32141929251340579101134755550 | -3.99625556818233899156012676115 | -3.71338229238713107344696781150 ] 
    x = [ 9.2291528841434270690474487613 | 4.89048495729257757968750924466 | 2.37515078613114432334368499533 | -2.95597861126272534034763855730 | -0.779711711749444046696825442115 | 3.27079149777109366194351751802 | -3.17521718364761224762308056898 | 2.68668720549406698517706084665 | 2.63130274924986834254275953881 | -4.62771440378819359403064072838 | -0.10935941819799870809205407330 | -3.43043871330863204530928708180 | -1.83466995045395930614920963998 | -1.94937220525768787093216996252 | -0.82103838453472674393704619217 | -6.40006949788119805862402865815 ] 
    v = [ 5.8509969265287577449565714862 | 4.64632649132342006075615151343 | 3.06059909758162487279772944410 | 3.72107894941210508288006455052 | 1.47329028276856552103069958246 | -1.38334386642316862568710056142 | 3.37815595761466932409087727508 | 0.24415846596915751893135773123 | -0.37391189208755788762066859745 | -1.08977620016223674033730501170 | -6.10100468655675911506134031085 | -5.01672563145802943293692809675 ] 
    u = [ 3.84832871290794002880078517680 | 3.69674772013849178367377716728 | 3.41613844231469874069313780918 | 2.83366682171973928970244228952 | 2.00266821362081771615578630940 | 0.02433122927361329920628738324 | -0.037982484700029416602260534095 | -3.92344302188197603003974730122 ] 
    w = [ 1.99985057333946511042561071830 | 1.99940231568618732031156829916 | 1.99760961997128825898004539475 | 1.99044419380183469986400450120 | 1.96186808863943569382335018421 | 1.84892639722175270848921687764 | 1.41852882234341048171309241443 | 0.012224019818983016291781808195 | -1.99985057333946511042561071830 ] 
    2*cos(  1*pi/257) = 1.99985057334  = 1.99985057333946511042561071830
    2*cos(  2*pi/257) = 1.99940231569  = 1.99940231568618732031156829916
    2*cos(  4*pi/257) = 1.99760961997  = 1.99760961997128825898004539475
    2*cos(  8*pi/257) = 1.9904441938   = 1.99044419380183469986400450120
    2*cos( 16*pi/257) = 1.96186808864  = 1.96186808863943569382335018421
    2*cos( 32*pi/257) = 1.84892639722  = 1.84892639722175270848921687764
    2*cos( 64*pi/257) = 1.41852882234  = 1.41852882234341048171309241443
    2*cos(128*pi/257) = 0.012224019819 = 0.012224019818983016291781808195
    2*cos(256*pi/257) = -1.99985057334 = -1.99985057333946511042561071830
    paw() = [1.0, 2.0, 4.0, 8.0, 16.0, 32.0, 64.0, 128.0, 256.0]

---
+++ pi257.py
@@ -0,0 +1,35 @@
+#!/usr/bin/env python -i
+
+# Calculating 
+
+from math import *;
+from decimal import Decimal as Dec, getcontext as gc; gc().prec=30; s = lambda r:Dec(r).sqrt()
+
+Q = lambda b,c:((b+s(b*b-4*c))/2, (b-s(b*b-4*c))/2)
+
+t0,t1 = Q(-1,-64); t = [t0, t1];
+(z0,z2), (z1,z3) = Q(t0,-16), Q(t1,-16); z = [z0,z1,z2,z3];
+
+Y = lambda z,t:Q(z, -5-t-2*z)
+(y0,y4), (y5,y1), (y2,y6), (y7,y3) = (Y(z0,t0), Y(z1,t1), Y(z2,t0), Y(z3,t1)); y = [y0,y1,y2,y3,y4,y5,y6,y7];
+
+X = lambda n:Q(y[n],t[n&1]+y[n]+y[(n+2)&7]+2*y[(n+5)&7]);
+(x0,x8),(x1,x9),(x2,x10),(x3,x11), (x4,x12),(x5,x13),(x14,x6),(x7,x15) = map(X, range(8)); x = [x0,x1,x2,x3, x4,x5,x6,x7, x8,x9,x10,x11, x12,x13,x14,x15];
+
+V = lambda n:Q(x[n], x[n]+x[(n+1)&15]+x[(n+2)&15]+x[(n+5)&15]);
+(v0,v6), (v1,v7), (v2,v8), (v3,v9), (v4,v10), (v5,v11) = map(V, [0,1,7,8,9,15]); v = [v0,v1,v2,v3,v4,v5,v6,v7,v8,v9,v10,v11];
+
+(u0,u4), (u1,u5), (u2,u6), (u3,u7) = Q(v0, v1+v2), Q(v3, v4+v5), Q(v6,v7+v8), Q(v9, v10+v11); u = [u0,u1,u2,u3,u4,u5,u6,u7]
+(w0,w4), (w1,w5), (w2,w6), (w3,w7) = Q(u0,u1), Q(u2,u3), Q(u4,u5), Q(u6,u7); w = [-w7,w0,w1,w2,w3,w4,w5,w6,w7]
+
+S = lambda a:"[ " + " | ".join(map(lambda x:str(x), a)) + " ] ";
+print 'Here is how to calculate real radical form expression for %s -' % ('cos(pi*%s/257)'%[2**n for n in range(9)]).replace(' ','')
+print "Calculate: t[2] => z[4] => y[8] => x[16] => v[12] => u[8] => w[9]";
+print "t = %s\nz = %s\ny = %s\nx = %s\nv = %s\nu = %s\nw = %s"%(S(t),S(z),S(y),S(x),S(v),S(u),S(w));
+#print "t = %s\nz = %s\ny = %s\nx = %s\nv = %s\nu = %s\nw = %s"%tuple([S(m) for m in [t,z,y,x,v,u,w]]);
+print '\n'.join(['%s = %-14s = %s'%('2*cos(%3s*pi/257)'%2**n,2*cos(pi/257*2**n),str(w[n])) for n in range(9)])
+
+def paw(): # print acos w/2 
+    print 'paw() =', [ round(acos(float(z)/2)*257/pi,12) for z in w if abs(z)<=2 ]
+
+paw()
